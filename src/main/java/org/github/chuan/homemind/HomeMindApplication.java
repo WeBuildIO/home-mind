716877c274a0e5feaee05614a93ce1d4ac8eca42
chuan <15512109007@163.com>
2025-11-14 17:31:15 +0800

    初始化。

---
+++ src/main/java/org/github/chuan/homemind/HomeMindApplication.java
@@ -0,0 +1,13 @@
+package org.github.chuan.homemind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeMindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeMindApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/github/chuan/homemind/HomeMindApplicationTests.java
@@ -0,0 +1,13 @@
+package org.github.chuan.homemind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeMindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
